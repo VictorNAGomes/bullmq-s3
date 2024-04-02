cb49eced1a8aede23418ef5beb4049ed54d31f8b
VictorNAGomes <dev.victorgomes@gmail.com>
2024-04-02 02:13:55 -0300

    feat: add bullmq

--- app/app.js
+++ app/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+import { Queue, Worker } from "bullmq";
+const queue = new Queue("Queue");
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
@@ -8,4 +10,13 @@ app.listen(port, () => {
 
 app.get("/", async (req, res) => {
   res.json(`Get successfully`);
+});
+
+app.post("/add-queue", async (req, res) => {
+  queue.add("test", { ...req.body });
+  res.json("Adicionado na fila");
+});
+
+const worker = new Worker("Queue", async (job) => {
+  return "processando";
 });
