1789091bcc10dca2f4a103100dec6fb488cbb23a
VictorNAGomes <dev.victorgomes@gmail.com>
2024-04-03 22:07:41 -0300

    feat: change type of project to module

--- app/app.js
+++ app/app.js
@@ -1,8 +1,11 @@
-const express = require("express");
+import express from "express";
 const app = express();
 const port = process.env.PORT || 3000;
-import { Queue, Worker } from "bullmq";
-const queue = new Queue("Queue");
+
+import { Queue } from "bullmq";
+export const redisOptions = { host: "redis", port: 6379 };
+
+const myQueue = new Queue("myQueue", { connection: redisOptions });
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
@@ -13,10 +16,10 @@ app.get("/", async (req, res) => {
 });
 
 app.post("/add-queue", async (req, res) => {
-  queue.add("test", { ...req.body });
+  myQueue.add("test", { ...req.body });
   res.json("Adicionado na fila");
 });
 
-const worker = new Worker("Queue", async (job) => {
-  return "processando";
-});
+// const worker = new Worker("Queue", async (job) => {
+//   return "processando";
+// });
