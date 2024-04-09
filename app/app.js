3157f44595bdbe870855bc4a43b84b7d5f143e5a
VictorNAGomes <dev.victorgomes@gmail.com>
2024-04-08 22:22:51 -0300

    feat: handle file uplaod

--- app/app.js
+++ app/app.js
@@ -1,9 +1,11 @@
 import express from "express";
-const app = express();
+import multer from "multer";
 const port = process.env.PORT || 3000;
-
 import { Queue } from "bullmq";
 export const redisOptions = { host: "redis", port: 6379 };
+
+const app = express();
+const upload = multer({ dest: "uploads/" });
 
 const myQueue = new Queue("myQueue", { connection: redisOptions });
 
@@ -15,9 +17,9 @@ app.get("/", async (req, res) => {
   res.json(`Get successfully`);
 });
 
-app.post("/add-queue", async (req, res) => {
-  myQueue.add("test", { ...req.body });
-  res.json("Adicionado na fila");
+app.post("/upload", upload.single("file"), async (req, res) => {
+  // myQueue.add("test", { ...req.body });
+  // res.json("Adicionado na fila");
 });
 
 // const worker = new Worker("Queue", async (job) => {
